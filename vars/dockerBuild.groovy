06d768c62dc4acfaf7c438d0b9d01845fd90126b
anjali <anjalishreya880@gmail.com>
2023-04-18 03:36:05 +0530

    final converting into image

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,10 @@
 def call(String project, String hubUser, String ImageTag){
-    sh """
+    sh "
       docker image build -t ${hubUser}/${project} .
-      docker image tag ${hubUser}/${project}  ${hubUser}/${project}:${ImageTag}
-      docker image tag ${hubUser}/${project}  ${hubUser}/${project}:latest
-
-
-    """
+    "
 }
+
+
+/*//docker image tag ${hubUser}/${project}  ${hubUser}/${project}:${ImageTag}
+      //docker image tag ${hubUser}/${project}  ${hubUser}/${project}:latest
+*/
